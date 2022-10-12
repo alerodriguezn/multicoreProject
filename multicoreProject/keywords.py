035d33e90ad55612ba61a4b94e6ee98701567a31
BrithannyAL <brijesus.12@gmail.com>
2022-10-12 09:00:29 -0600

    nuevas keywords

--- multicoreProject/keywords.py
+++ multicoreProject/keywords.py
@@ -9,7 +9,15 @@ palabras_claves = {
 "servicios_streaming" : ("movie","movies","series","tv","soap opera","suscription","season","episode","pelicula","peliculas","television","novelas","temporada","suscripcion","capitulo"),
 
 
-"tienda_ropa" : ("shoes","camisas","pantalones","vestidos","products","moda","hombres","mujeres","men","women","woman","t-shirt","collection","collecion")
+"tienda_ropa" : ("shoes","camisas","pantalones","vestidos","products","moda","hombres","mujeres","men","women","woman","t-shirt","collection","collecion", "camisa", "pantalón" "vestido", "blusa", "interior", "falda", "short", "abrigo", "sueter", "medias", "bufanda", "tela", "talla", "oferta", "marca", "boutique", "ropa", "color", "diseño", "material", "zapatos", "tacones", "deportivo", "gala", "material", "bolso", "accesorios", "vestir", "mezclilla", "piezas", "traje", "shirt", "pants" "dress", "blouse", "underwear", "skirt", "short", "coat", "sweater", "stockings", "scarf", "cloth", "size" , "offer", "brand", "boutique", "clothing", "color", "design", "material", "shoes", "heels", "sporty", "gala", "material", " bag", "accessories", "dress", "denim", "pieces", "suit"),
 
+
+"plataforma_peliculas" : ("peliculas", "movies", "cine", "estreno", "cartelera", "clásicos", "género", "trailer", "reseña", "play", "maratón", "sagas", "dinastía", "director", "reparto", "resumen", "palomitas", "sinopsis", "obra", "personajes", "trama", "series", "taquilla", "animadas", "peliculas", "movies", "cinema", "premiere", "billboard", "classics", "genre", "trailer", "review", "play", "marathon", "sagas", "dynasty ", "director", "cast", "summary", "popcorn", "synopsis", "play", "characters", "plot", "series", "box office", "animation"),
+
+
+"redes_comunicacion" : ("mensajes", "notificación", "anuncio", "contenido", "cuenta", "usuario", "historia", "comentario", "amigos", "seguidores", "seguidos", "contraseña", "preferencias", "publicación", "compartir", "chat", "bucador", "inicio", "perfil", "reacción", "fotos", "videos", "like", "cuenta" "messages", "notification", "ad", "content", "account", "user", "story", "comment", "friends", "followers", "followed", "password", "preferences ", "post", "share", "browser", "home", "profile", "reaction", "photos", "videos", "account"),
+
+
+"books" : ("genero", "libro", "portada", "título", "sinopsis", "autor", "clasico", "best", "seller", "editorial", "edición", "sagas", "pasta", "capítulos", "contenido", "reseñas", "dedicatoria", "páginas", "librería", "oferta", "paquete", "combo", "educación", "educativos", "literatura", "separador", "genre", "book", "cover", "title", "synopsis", "author", "classic", "publisher", "sagas", "chapters", "content ", "reviews", "dedication", "pages", "bookstore", "offer", "package", "education", "educational", "literature")
 
 }
